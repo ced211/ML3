0ed1268cbe2fa694c060baf108a1c4a360a609bb
ced211 <43910066+ced211@users.noreply.github.com>
2018-12-17 15:16:23 +0100

    mise au propre

--- NLP.py
+++ NLP.py
@@ -6,60 +6,10 @@ import time
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler  
 from sklearn.neural_network import MLPRegressor
-from toy_example import measure_time, load_from_csv, build_rating_matrix, create_learning_matrices2,create_learning_matrices3, make_submission
+from toy_example import measure_time, load_from_csv, build_rating_matrix, create_learning_matrices4,create_learning_matrices3, make_submission
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
-
-
-def learning_mat4(movie,user,rating_matrix,user_movie_pair):
-    M = np.zeros((max(user)+1,19,5))
-    feature = np.zeros((user_movie_pair.shape[0],5))
-    for u,m in user_movie_pair:
-        if rating_matrix[u,m] != 0:
-            M[user[u-1],movie[m-1],rating_matrix[u,m]-1] += 1
-    i = 0
-    for u,m in user_movie_pair:
-        feature[i] = M[user[u-1],movie[m-1],:]
-        i += 1
-    print(feature)
-    return np.concatenate((create_learning_matrices2(rating_matrix,user_movie_pair),feature),axis=1)
-
-def create_movie_matrix(movie):
-    movie = movie[:,5:]
-    total = np.sum(movie,axis=0)
-    permutation = np.flip(np.argsort(total),axis=0)
-    movie = movie[:,permutation]
-    output = movie[:,0]
-    for j in range(movie.shape[0]):
-        i = 0
-        while movie[j][i] == 0:
-            i += 1
-        output[j] = i
-    return output
-
-def create_user(user):
-    #catgorie depend on occupation and gender
-    user = user[:,(1,2,3)]
-    categorieID = 0
-    categorie = {}
-    output = user[:,0]
-    i = 0
-    user_per_categorie = []
-    for u in user:
-        key = str(int(u[0]/10))+u[1]+u[2]
-        if key not in categorie:
-            categorie[key] = categorieID
-            output[i] = categorieID
-            user_per_categorie.append(1)            
-            categorieID += 1
-        else:
-            output[i] = categorie[key]
-            user_per_categorie[categorie[key]] += 1
-        i += 1  
-    print(categorie)
-    print(user_per_categorie)
-    return output
 
 def y_check(y):
     for i in range(len(y)):
@@ -83,13 +33,9 @@ if __name__ == '__main__':
     with measure_time('Build matrices'):
         print("Building matrices ...")
         # Build the learning matrix
-        users = create_user(users)
-        movies = create_movie_matrix(movies)
         user_movie_rating_triplets = np.hstack((training_user_movie_pairs,training_labels.reshape((-1, 1))))
         rating_matrix = build_rating_matrix(user_movie_rating_triplets)
-        X_ls = learning_mat4(movies,users,rating_matrix,training_user_movie_pairs)
-        print(X_ls.shape)
-
+        X_ls = create_learning_matrices4(movies,users,rating_matrix,training_user_movie_pairs)
     # Build the model
     y_ls = training_labels
 	#creation of the learning and validation set.
@@ -100,9 +46,9 @@ if __name__ == '__main__':
     X_vs = scaler.transform(X_vs)
     start = time.time()
     score = []
-    parameters = [(100,100,100)]
+    parameters = [(100,50)]
     for parameter in parameters:
-        model = MLPRegressor(hidden_layer_sizes=parameter,random_state = 20,early_stopping = True)
+        model = MLPRegressor(hidden_layer_sizes=parameter,random_state = 20)
         with measure_time('Training'):
             print('Training...')
             model.fit(X_ls2, y_ls2)
@@ -117,7 +63,7 @@ if __name__ == '__main__':
     print(best)
     with measure_time('Training'):
         print("training....")
-        model = MLPRegressor(hidden_layer_sizes=(100,100,100),random_state = 20)
+        model = MLPRegressor(hidden_layer_sizes=(100,50),random_state = 20)
         scaler.fit(X_ls)
         X_ls = scaler.transform(X_ls)
         model.fit(X_ls, y_ls)
@@ -126,15 +72,11 @@ if __name__ == '__main__':
     test_user_movie_pairs = load_from_csv(os.path.join(prefix, 'data_test.csv'))
 
     # Build the prediction matrix
-    X_ts = learning_mat4(movies,users,rating_matrix,test_user_movie_pairs)
+    X_ts = create_learning_matrices4(movies,users,rating_matrix,test_user_movie_pairs)
     X_ts = scaler.transform(X_ts)
     # Predict
     y_pred = model.predict(X_ts)    
-    print(y_pred)
-
-    print(max(y_pred))
     y_pred = y_check(y_pred)
-    print(max(y_pred))
     # Making the submission file
     fname = make_submission(y_pred, test_user_movie_pairs, 'NLP' + "age_sexe_metier")
     print('Submission file "{}" successfully written'.format(fname))
