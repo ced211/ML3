dbbb42b4f10e423dbadab27d4e425a02d97a0150
Pseudolesss <j.lhoest@student.uliege.be>
2018-12-17 13:12:04 +0100

    Mise au propre de create_matrix3

--- toy_example.py
+++ toy_example.py
@@ -118,8 +118,8 @@ def create_learning_matrices(rating_matrix, user_movie_pairs):
 
 def create_learning_matrices2(rating_matrix,user_movie_pairs):
     """create the learning matrice [sn_sample,10] from the rating_matrix, and the user movie pair.
-        the feature vector is composed with the total number of rank1 given by the user, total of rank2 given by the user, ...
-        the total of rank 1 received by the movie, the total of rank 2 received by the movie, ..."""
+        the feature vector is composed with the total number of rank1 given by the user, total of rank2 given
+        by the user, ... the total of rank 1 received by the movie, the total of rank 2 received by the movie, ..."""
     with measure_time('Building learning matrix'):
         #create intermediate matrix
         users_rank = np.zeros((rating_matrix.shape[0],5))
@@ -173,20 +173,99 @@ def create_user(user):
     return output
 
 def create_learning_matrices3(User_movie_pairs):
-    ret = list()
+    """
+        Create the learning matrix `X` from the `rating_matrix`.
+
+        If `u, m = user_movie_pairs[i]`, then X[i] is the feature vector
+        corresponding to user `u` and movie `m`. The feature vector is composed
+        of `n_user_features_kept + n_movie_features_kept` features. The features
+        kept depend of the beginning of this method where removed features are specified.
+
+        In other words, the feature vector for a pair (user, movie) is the
+        concatenation of values of the corresponding user features and values
+        of the corresponding movie features.
+
+        Parameters
+        ----------
+        User_movie_pairs: array [n_predictions, 2]
+            If `u, m = user_movie_pairs[i]`, the i-th raw of the learning matrix
+            must relate to user `u` and movie `m`
+
+        Return
+        ------
+        ret: numpy array with n_predictions rows and n_features_kept columns
+    """
+
+    X = list()
     prefix = 'data/'
     user = load_from_csv(os.path.join(prefix, 'data_user.csv'))
     movie = load_from_csv(os.path.join(prefix, 'data_movie.csv'))
-    for (x, y) in User_movie_pairs:
-        tmp = np.append(np.delete(user[x-1], [0, 2,3, 4]), np.delete(movie[y-1], [0, 1, 2, 3, 4])) # remove colums of specified indexes and append both ressult in tmp
 
+    # Headers of both cvs file
+    userHeader = pd.read_csv(os.path.join(prefix, 'data_user.csv'), nrows=1).columns
+    movieHeader = pd.read_csv(os.path.join(prefix, 'data_movie.csv'), nrows=1).columns
+    u = dict()
+    m = dict()
+
+    # Associating header names to corresponding indices in the user and movie matrix
+    k = 0
+    for i in userHeader:
+        u[i] = k
+        k += 1
+    k = 0
+    for i in movieHeader:
+        m[i] = k
+        k += 1
+
+    # lists of features to delete represented by their indexes
+    us = list()
+    ms = list()
+
+    # Features removed in user matrix
+    us.append(u["user_id"])
+
+    # Features removed in movie matrix
+    ms.append(m["movie_id"])
+    ms.append(m["movie_title"])
+
+    # ms.append(m["release_date"])
+
+    ms.append(m["video_release_date"])
+    ms.append(m["IMDb_URL"])
+    # ms.append(m["unknown"])
+    # ms.append(m["Fantasy"])
+    # ms.append(m["Film-Noir"])
+
+    # us.append(u["gender"])
+    # us.append(u["occupation"])
+    # us.append(u["zip_code"])
+
+    for (x, y) in User_movie_pairs:
+
+        # Handle unknown row exception
+        if movie[y - 1][m["movie_title"]] == "unknown":
+            movie[y - 1][m["release_date"]] = 0
+
+        if isinstance(movie[y - 1][m["release_date"]], str):
+            movie[y - 1][m["release_date"]] = int(movie[y - 1][2].split("-")[-1])  # Extract year release as int
+
+        user[x - 1][u["zip_code"]] = user[x - 1][u["zip_code"]][:2]  # extract 2 first letter of zip code
+
+        # remove columns given indexes and append both results in tmp
+        tmp = np.append(np.delete(user[x - 1], us),
+                        np.delete(movie[y - 1], ms))
+
+        # Hashing of string data into int to be processed by the models
         for i in range(len(tmp)):
             if isinstance(tmp[i], str):
                 tmp[i] = hash(tmp[i])
 
-        ret.append(tmp)
+        X.append(tmp)
 
-    return np.asarray(ret)
+    X = np.asarray(X)
+
+    return X
+
 
 def create_learning_matrices4(movie,user,rating_matrix,user_movie_pair):
     movie = create_movie(movie)
